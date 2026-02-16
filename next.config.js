468bc71abb2d845a3ad4f80d42e5e5d37256a9bf
JerrySea <mac@MacBook-Air2022216.local>
2026-02-17 00:44:32 +0800

    fix: 使用 CommonJS 格式的 next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-import withPWA from 'next-pwa';
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -14,9 +19,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
+module.exports = withPWA(nextConfig);
