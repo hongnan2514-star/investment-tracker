f3e4f9360fe2ed334059d62b256412bac8263d8a
JerrySea <mac@192.168.1.4>
2026-02-27 18:45:33 +0800

    “修改next.config.js”

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// next.config.js
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
@@ -7,13 +8,19 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverExternalPackages: ['ccxt', 'protobufjs'],
+  serverExternalPackages: ['ccxt', 'protobufjs'],
   turbopack: {},
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com', // 新增
         port: '',
         pathname: '/**',
       },
